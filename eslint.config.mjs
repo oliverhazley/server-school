7e08f263c580617a21dd8117df61cf7521fa68e7
olive <oliver.hazley@gmail.com>
2025-01-15 14:08:57 +0200

    commit finnished homework

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import js from '@eslint/js';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {...globals.node},
+    },
+  },
+  js.configs.recommended,
 ];
